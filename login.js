6851a4ce8318deafd668683b3696c40e78ea222d
Gouravdevrale <116137382+Gouravdevrale@users.noreply.github.com>
2023-11-22 14:07:28 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,7 @@
+function login() {
+    // Implement login logic here (e.g., send an AJAX request to the server).
+}
+
+function signup() {
+    // Implement sign-up logic here (e.g., send an AJAX request to the server).
+}
